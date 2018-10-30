001bb36ebe947bb15955e59fef4b4511c8aac8c9
Steven Hedges <steven.hedges34@gmail.com>
2018-10-30 12:14:10 -0400

    Canvas is added and works

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 // main JavaScript file
+
+// Need to access the canvas
+const canvas = document.getElementById('canvas');
+// Need the context so we can draw to the canvas
+const context = canvas.getContext('2d');
+
+context.fillStyle = '#000';
+context.fillRect(0, 0, canvas.width, canvas.height);
+
+// var mapWidth = map[0].length;
+// var mapHeight = map.length;
+// var miniMapScale = 8;
+
+// function init() {
+
+// 	drawMiniMap();
+// }
