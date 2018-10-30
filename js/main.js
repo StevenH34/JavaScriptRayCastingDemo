aa34630b06b12ad89f1e5b257ea17f91ec94749d
Steven Hedges <steven.hedges34@gmail.com>
2018-10-30 14:36:09 -0400

    Minimap is now visible in the browser

--- js/main.js
+++ js/main.js
@@ -1,18 +1,49 @@
 // main JavaScript file
 
-// Need to access the canvas
-const canvas = document.getElementById('canvas');
-// Need the context so we can draw to the canvas
-const context = canvas.getContext('2d');
-
-context.fillStyle = '#000';
-context.fillRect(0, 0, canvas.width, canvas.height);
+let mapWidth = map[0].length;
+let mapHeight = map.length;
+const miniMapScale = 8;
 
 // var mapWidth = map[0].length;
 // var mapHeight = map.length;
 // var miniMapScale = 8;
 
-// function init() {
 
-// 	drawMiniMap();
-// }
+
+
+function drawMiniMap() {
+    // Need to access the canvas
+    let minimapCanvas = document.getElementById('minimap');
+    
+    // resize the internal canvas dimensions 
+    minimapCanvas.width = mapWidth * miniMapScale;	
+    minimapCanvas.height = mapHeight * miniMapScale;
+
+    // resize the canvas css?
+
+    // Need the context so we can draw to the canvas
+    let minimapContext = minimapCanvas.getContext('2d');
+
+    // minimapContext.fillStyle = '#000';
+    // minimapContext.fillRect(0, 0, minimapCanvas.width, minimapCanvas.height);
+
+    for (var y=0;y<mapHeight;y++) {
+		for (var x=0;x<mapWidth;x++) {
+
+			var wall = map[y][x];
+
+			if (wall > 0) { // if there is a wall block at this (x,y) ...
+
+				minimapContext.fillStyle = "rgb(200,200,200)";
+				minimapContext.fillRect(				// ... then draw a block on the minimap
+					x * miniMapScale,
+					y * miniMapScale,
+					miniMapScale,miniMapScale
+				);
+
+			}
+		}
+	}
+
+}
+drawMiniMap();
