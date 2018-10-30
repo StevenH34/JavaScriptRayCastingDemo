924d2a3cb33cc63fe584ec95c187961209e1818b
Steven Hedges <steven.hedges34@gmail.com>
2018-10-30 14:43:05 -0400

    cleaned up some of the code

--- js/main.js
+++ js/main.js
@@ -1,36 +1,22 @@
 // main JavaScript file
 
+const minimapCanvas = document.getElementById('minimap');   // Need to access the canvas
+const minimapContext = minimapCanvas.getContext('2d');      // Need to acces the context of the canvas
+
 let mapWidth = map[0].length;
 let mapHeight = map.length;
 const miniMapScale = 8;
 
-// var mapWidth = map[0].length;
-// var mapHeight = map.length;
-// var miniMapScale = 8;
-
-
-
-
 function drawMiniMap() {
-    // Need to access the canvas
-    let minimapCanvas = document.getElementById('minimap');
     
-    // resize the internal canvas dimensions 
+    // resize the internal canvas dimensions to the map dimensions
     minimapCanvas.width = mapWidth * miniMapScale;	
     minimapCanvas.height = mapHeight * miniMapScale;
 
-    // resize the canvas css?
+    for (let y = 0 ; y < mapHeight; y++) {
+		for (let x = 0; x < mapWidth; x++) {
 
-    // Need the context so we can draw to the canvas
-    let minimapContext = minimapCanvas.getContext('2d');
-
-    // minimapContext.fillStyle = '#000';
-    // minimapContext.fillRect(0, 0, minimapCanvas.width, minimapCanvas.height);
-
-    for (var y=0;y<mapHeight;y++) {
-		for (var x=0;x<mapWidth;x++) {
-
-			var wall = map[y][x];
+			let wall = map[y][x];
 
 			if (wall > 0) { // if there is a wall block at this (x,y) ...
 
